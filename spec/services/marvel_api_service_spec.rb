1c86991f3ef30e72288f999a01e5ed058d210b3f
Anatolii Borysov <borisano@gmail.com>
2024-02-20 12:29:33 +0000

    update specs

--- spec/services/marvel_api_service_spec.rb
+++ spec/services/marvel_api_service_spec.rb
@@ -1,40 +1,40 @@
 require 'rails_helper'
 require 'net/http'
 
-RSpec.describe MarvelApiService, type: :service do
+RSpec.describe MarvelApiService do
   describe '.fetch' do
+    let(:page) { 1 }
     let(:limit) { 15 }
-    let(:page) { 0 }
+    let(:offset) { (page - 1) * limit }
+    let(:url) { "https://gateway.marvel.com:443/v1/public/comics?orderBy=issueNumber&limit=#{limit}&offset=#{offset}&ts=#{ts}&apikey=#{MarvelApiService::MARVEL_PUBLIC_KEY}&hash=#{hash}" }
+    let(:ts) { Time.now.to_i.to_s }
+    let(:hash) { MarvelApiService.hash(ts) }
+    let(:response_body) { { 'data' => { 'results' => [{ 'title' => 'Comic 1', 'description' => 'Description 1', 'images' => [{ 'path' => 'path1', 'extension' => 'jpg' }] }] } }.to_json }
+    let(:response) { instance_double(Net::HTTPSuccess, body: response_body) }
 
-    context 'when API request is successful' do
-      let(:offset) { page * limit }
-      let(:url) { MarvelApiService.send(:build_url, page, limit) }
-      let(:response_body) { { 'data' => { 'results' => [{ 'title' => 'Comic 1', 'issueNumber' => 1, 'images' => [{ 'path' => 'path', 'extension' => 'jpg' }], 'dates' => [] }] } }.to_json }
-      let(:response) { Net::HTTPSuccess.new('1.1', '200', 'OK') }
-      let(:expected_comics) { [{ title: 'Comic 1', issue_number: 1, cover_url: 'path.jpg', dates: [] }] }
+    before do
+      allow(Net::HTTP).to receive(:start).and_return(response)
+    end
 
-      # before do
-      #   allow(MarvelApiService).to receive(:make_api_request).with(url).and_return(response)
-      #   allow(response).to receive(:body).and_return(response_body)
-      # end
+    context 'when the API request is successful' do
+      it 'returns an array of comics' do
+        comics = MarvelApiService.fetch(page, limit)
 
-      it 'returns the chronological comics' do
-        comics = MarvelApiService.fetch(limit, page)
-        expect(comics).to eq(expected_comics)
+        expect(comics).to be_an(Array)
+        expect(comics.size).to eq(1)
+        expect(comics.first).to include(title: 'Comic 1', description: 'Description 1', cover_url: 'path1.jpg')
       end
     end
 
-    context 'when API request fails' do
-      let(:offset) { page * limit }
-      let(:url) { MarvelApiService.send(:build_url, page, limit) }
-      let(:response) { Net::HTTPBadRequest.new('1.1', '400', 'Bad Request') }
+    context 'when the API request fails' do
+      let(:response) { instance_double(Net::HTTPResponse, code: '500') }
 
       before do
-        allow(MarvelComicsFetcher).to receive(:make_api_request).with(url).and_return(response)
+        allow(response).to receive(:code).and_return("500")
       end
 
       it 'prints an error message' do
-        expect { MarvelApiService.fetch(limit, page) }.to output("Error fetching comics: #{response.code}\n").to_stdout
+        expect { MarvelApiService.fetch(page, limit) }.to output("Error fetching comics: 500\n").to_stdout
       end
     end
   end
