58a094239edd668146b2c14fee44810adb538dff
Anatolii Borysov <borisano@gmail.com>
2024-02-20 12:29:52 +0000

    Add API fetching functionality (#3)
    * First iteration of API fetching
    
    * Rename fetching method
    
    * Refactoring
    
    * Properly fetch images
    
    * update specs

--- app/services/marvel_api_service.rb
+++ app/services/marvel_api_service.rb
@@ -1,5 +1,70 @@
+require 'net/http'
+require 'uri'
+require 'json'
+require 'digest/md5'
+
 class MarvelApiService
-  def self.get_comics
-    true
+
+  # Replace with your Marvel API keys
+  MARVEL_PUBLIC_KEY = ENV['MARVEL_PUBLIC_KEY']
+  MARVEL_PRIVATE_KEY = ENV['MARVEL_PRIVATE_KEY']
+
+  def self.fetch(page = 1,limit = 15)
+    offset = (page - 1) * limit # so that page 1 starts at offset 0, page 2 at 15, etc.
+    comics = []
+
+    url = build_url(offset, limit)
+    response = make_api_request(url)
+
+    if response.is_a?(Net::HTTPSuccess)
+      data = JSON.parse(response.body)
+      comics = extract_comics(data)
+      #comics.sort_by { |comic| comic['dates'].first['date'] }
+    else
+      puts "Error fetching comics: #{response.code}"
+    end
+
+    comics
+  end
+
+  private
+
+  def self.hash(ts)
+    Digest::MD5.hexdigest(ts + MARVEL_PRIVATE_KEY + MARVEL_PUBLIC_KEY)
+  end
+
+  def self.build_url(offset, limit)
+    ts = Time.now.to_i.to_s
+    uri = URI("https://gateway.marvel.com:443/v1/public/comics")
+    uri.query = "orderBy=issueNumber&limit=#{limit}&offset=#{offset}&ts=#{ts}&apikey=#{MARVEL_PUBLIC_KEY}&hash=#{self.hash(ts)}"
+    uri
+  end
+
+  def self.make_api_request(url)
+    uri = URI(url)
+    req = Net::HTTP::Get.new(uri)
+    req['User-Agent'] = "My Ruby App"
+    Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(req)
+    end
+  end
+
+  def self.extract_comics(data)
+    data['data']['results'].map do |comic|
+      {
+        title: comic['title'],
+        description: comic['description'],
+        cover_url: self.extract_cover(comic),  #comic['images']&.first&.fetch('path', '') + '.' + comic['images']&.first&.fetch('extension', ''),
+      }
+    end
+  end
+
+  def self.extract_cover(comic)
+    image = comic['images'].first
+    if image.present? # if image is present, return the full url string extension
+      image['path'] + '.' + image['extension']
+    else # if image is not present, return the 'no image'
+      'no_image.png'
+    end
   end
 end
--- spec/services/marvel_api_service_spec.rb
+++ spec/services/marvel_api_service_spec.rb
@@ -1,9 +1,41 @@
 require 'rails_helper'
+require 'net/http'
 
-RSpec.describe MarvelApiService, type: :service do
-  describe '.get_comics' do
-    it 'fails' do
-      expect(described_class.get_comics).to eq(true)
+RSpec.describe MarvelApiService do
+  describe '.fetch' do
+    let(:page) { 1 }
+    let(:limit) { 15 }
+    let(:offset) { (page - 1) * limit }
+    let(:url) { "https://gateway.marvel.com:443/v1/public/comics?orderBy=issueNumber&limit=#{limit}&offset=#{offset}&ts=#{ts}&apikey=#{MarvelApiService::MARVEL_PUBLIC_KEY}&hash=#{hash}" }
+    let(:ts) { Time.now.to_i.to_s }
+    let(:hash) { MarvelApiService.hash(ts) }
+    let(:response_body) { { 'data' => { 'results' => [{ 'title' => 'Comic 1', 'description' => 'Description 1', 'images' => [{ 'path' => 'path1', 'extension' => 'jpg' }] }] } }.to_json }
+    let(:response) { instance_double(Net::HTTPSuccess, body: response_body) }
+
+    before do
+      allow(Net::HTTP).to receive(:start).and_return(response)
+    end
+
+    context 'when the API request is successful' do
+      it 'returns an array of comics' do
+        comics = MarvelApiService.fetch(page, limit)
+
+        expect(comics).to be_an(Array)
+        expect(comics.size).to eq(1)
+        expect(comics.first).to include(title: 'Comic 1', description: 'Description 1', cover_url: 'path1.jpg')
+      end
+    end
+
+    context 'when the API request fails' do
+      let(:response) { instance_double(Net::HTTPResponse, code: '500') }
+
+      before do
+        allow(response).to receive(:code).and_return("500")
+      end
+
+      it 'prints an error message' do
+        expect { MarvelApiService.fetch(page, limit) }.to output("Error fetching comics: 500\n").to_stdout
+      end
     end
   end
 end
