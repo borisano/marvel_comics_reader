69ba88281d7378aabfbc832027fd13bbddcb077e
Anatolii Borysov <borisano@gmail.com>
2024-02-20 12:34:54 +0000

    Add turbo frame for comics

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
     @comics = MarvelApiService.fetch
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 
   def about
