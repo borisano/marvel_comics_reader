11658c54c9264e1a0fd730750ed706274765c683
Anatolii Borysov <borisano@gmail.com>
2024-02-22 16:49:56 +0000

    Implement caching for MarvelAPI response

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 class HomeController < ApplicationController
   before_action :set_page, only: [:index]
   before_action  :set_per_page, only: [:index]
+  before_action :set_search, only: [:index]
 
   def index
-    @search = params[:search]
-    @comics = MarvelApiService.fetch(page: @page, limit: @per_page, search: @search)
+    # Cache to speed things up and (mainly), to avoid hitting the Marvel API rate limit
+    cache_key = "marvel_characters_#{@page}_#{@per_page}_#{@search}"
+    @comics = Rails.cache.fetch(cache_key, expires_in: 1.hour) do
+      MarvelApiService.fetch(page: @page, limit: @per_page, search: @search)
+    end
 
     respond_to do |format|
       format.html
@@ -22,5 +26,9 @@ class HomeController < ApplicationController
   def set_per_page
     @per_page = params[:per_page].to_i
     @per_page = 15 if @per_page < 1
+  end
+
+  def set_search
+    @search = params[:search]
   end
 end
